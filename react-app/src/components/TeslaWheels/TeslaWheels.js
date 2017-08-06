5e3e2ccc2ca719d6ed57e9cc4677a3341e1a0e3f
Anderson Nunes <anderson.nunes1988@gmail.com>
2017-08-06 20:33:24 -0300

    Building Tesla’s Battery Range Calculator with React (Part 2: Redux version)

--- react-app/src/components/TeslaWheels/TeslaWheels.js
+++ react-app/src/components/TeslaWheels/TeslaWheels.js
@@ -4,7 +4,7 @@ import './TeslaWheels.css';
 
 const LabelLists = (props) => {
   const value = props.wheels.value;
-  const changeHandler = props.wheels.handleChangeWheels;
+  const changeHandler = props.wheels.changeHandler;
   const sizes = [19, 21];
   const LabelItems = sizes.map(size => (
     <label key={size} className={`tesla-wheels__item tesla-wheels__item--${size} ${value === size ? 'tesla-wheels__item--active' : '' }`}>
